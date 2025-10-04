ad2e4c6a682785cbe124f803d28d72b20febe881
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2025-10-04 02:35:49 +0200

    Add gizmos debug ui

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -1,6 +1,7 @@
 //! Development tools for the game. This plugin is only enabled in dev builds.
 
 use crate::screens::Screen;
+use avian2d::prelude::{PhysicsDebugPlugin, PhysicsGizmos};
 use bevy::{
     dev_tools::{
         fps_overlay::{
@@ -18,10 +19,12 @@ use bevy_inspector_egui::{
     quick::{StateInspectorPlugin, WorldInspectorPlugin},
 };
 
+const TOGGLE_UI_KEY: KeyCode = KeyCode::F4;
+const TOGGLE_GIZMOS_KEY: KeyCode = KeyCode::F3;
+
 #[derive(States, Reflect, Clone, Copy, PartialEq, Eq, Hash, Debug)]
 #[reflect(State)]
 struct InspectorEnabled(bool);
-
 impl Default for InspectorEnabled {
     fn default() -> Self {
         InspectorEnabled(true)
@@ -47,20 +50,28 @@ pub(super) fn plugin(app: &mut App) {
             .run_if(in_state(InspectorEnabled(true))),
         StateInspectorPlugin::<Screen>::default()
             .run_if(in_state(InspectorEnabled(true))),
-    ));
+        PhysicsDebugPlugin,
+    ))
+    .insert_gizmo_config(PhysicsGizmos::default(), GizmoConfig {
+        enabled: false,
+        ..default()
+    });
 
-    // Log `Screen` state transitions.
-    app.add_systems(Update, log_transitions::<Screen>);
-
-    // Toggle the debug overlay for UI.
-    app.add_systems(
-        Update,
-        toggle_debug_ui.run_if(input_just_pressed(TOGGLE_KEY)),
-    );
+    app.add_systems(Update, log_transitions::<Screen>)
+        .add_systems(
+            Update,
+            toggle_debug_ui.run_if(input_just_pressed(TOGGLE_UI_KEY)),
+        )
+        .add_systems(
+            Update,
+            toggle_gizmos.run_if(input_just_pressed(TOGGLE_GIZMOS_KEY)),
+        );
 }
-
-const TOGGLE_KEY: KeyCode = KeyCode::Backquote;
 
 fn toggle_debug_ui(mut options: ResMut<UiDebugOptions>) {
     options.toggle();
+}
+
+fn toggle_gizmos(mut gizmo_configs: ResMut<GizmoConfigStore>) {
+    gizmo_configs.config_mut::<PhysicsGizmos>().0.enabled ^= true;
 }
