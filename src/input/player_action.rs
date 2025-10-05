b744eda04d02366c952972392307d62f1c48dac5
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2025-10-05 08:12:30 +0200

    Split PlayerAction into sub-module

---
+++ src/input/player_action.rs
@@ -0,0 +1,36 @@
+use bevy::prelude::*;
+use leafwing_input_manager::prelude::*;
+
+pub(super) fn plugin(app: &mut App) {
+    app.add_plugins(InputManagerPlugin::<PlayerAction>::default())
+        .init_resource::<ActionState<PlayerAction>>()
+        .insert_resource(PlayerAction::default_input_map());
+}
+
+#[derive(Actionlike, Reflect, Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub enum PlayerAction {
+    #[actionlike(DualAxis)]
+    Move,
+    #[actionlike(DualAxis)]
+    Aim,
+    #[actionlike(Button)]
+    Shoot,
+}
+
+impl PlayerAction {
+    fn default_input_map() -> InputMap<PlayerAction> {
+        use PlayerAction::*;
+        InputMap::default()
+            .with_dual_axis(Move, VirtualDPad::wasd())
+            .with_dual_axis(Move, VirtualDPad::dpad())
+            .with_dual_axis(Move, GamepadStick::LEFT)
+            .with_dual_axis(Aim, VirtualDPad::arrow_keys())
+            .with_dual_axis(Aim, VirtualDPad::action_pad())
+            .with_dual_axis(Aim, GamepadStick::RIGHT)
+            .with(Shoot, KeyCode::Space)
+            .with(Shoot, GamepadButton::RightTrigger)
+            .with(Shoot, GamepadButton::RightTrigger2)
+            .with(Shoot, GamepadButton::LeftTrigger)
+            .with(Shoot, GamepadButton::LeftTrigger2)
+    }
+}
